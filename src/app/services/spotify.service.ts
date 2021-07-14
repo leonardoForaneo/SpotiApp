c58b699feab06e42e0c1b3ea6c7d280016427655
leonardoForaneo <leonardomartinez.contact@gmail.com>
2021-07-14 12:50:18 -0500

    Centralizando las peticiones: con una funcion para no repetir

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -19,29 +19,14 @@ export class SpotifyService {
   }
 
   getNewReleases() {
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQBSwGv2rK1FWnmefVgjHbjQfeZbcNMhxLPfIRO01w0kn5g0Zjf7rJ6vd3t8u9Hhb8gmeAH9PiJGfaVz-jw',
-    });
-    return this.http
-      .get('https://api.spotify.com/v1/browse/new-releases', {
-        headers,
-      })
-      .pipe(map((data: any) => data['albums'].items));
+    return this.getQuery('browse/new-releases?limit=20').pipe(
+      map((data: any) => data['albums'].items)
+    );
   }
 
   getArtista(termino: string) {
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQBSwGv2rK1FWnmefVgjHbjQfeZbcNMhxLPfIRO01w0kn5g0Zjf7rJ6vd3t8u9Hhb8gmeAH9PiJGfaVz-jw',
-    });
-    return this.http
-      .get(
-        `https://api.spotify.com/v1/search?query=${termino}&type=artist&offset=0&limit=15`,
-        {
-          headers,
-        }
-      )
-      .pipe(map((data: any) => data['artists'].items));
+    return this.getQuery(
+      `search?query=${termino}&type=artist&offset=0&limit=15`
+    ).pipe(map((data: any) => data['artists'].items));
   }
 }
